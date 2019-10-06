5cb74f1baa9ef8d01eda5c6a44f40fc7b8c0c8f9
Neeraj Tandon <neerajtandon25@gmail.com>
2019-10-06 12:57:07 -0500

    add graphdata script and graphdata

--- R/1_explore_data.R
+++ R/1_explore_data.R
@@ -1,31 +1,41 @@
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(jsonlite)
 
+"https://coolors.co/f4e8c1-a0c1b9-70a0af-706993-331e38"
 
-library(maps)
-library(maptools)
-library(rgdal)
+graph_dat <- dat %>% 
+ select(venue_uid, artist_uid) %>%
+ group_by(venue_uid, artist_uid) %>%
+ summarize(num_visits=n()) %>%
+ mutate(venue_name = paste0("venue-", venue_uid),
+        artist_name = paste0("artist-", artist_uid)) %>%
+ filter(!is.na(artist_uid)) %>%
+arrange(-num_visits, venue_uid, artist_uid) %>%
+data.frame()
 
-#library(tidycensus)
-library(data.table)
+## Write connections
 
-dat <- fread('build/dat.csv')
+connection_names <- c(source="artist_name", target="venue_name", value="num_visits")
+graph_dat %>%
+    select(connection_names) %>%
+    jsonlite::toJSON(auto_unbox=T) %>% 
+    write('build/connections-data.json')
 
-venue_counts <- dat %>%
-                group_by(venue_uid) %>%
-                summarize(venue_count = n(),
-                          lat = min(lat),
-                          lng = min(lng))
+artist_nodes <- data.frame(
+                            id=unique(graph_dat$artist_name),
+                            group=1
+                )
+artist_nodes %>%
+    jsonlite::toJSON(auto_unbox=T) %>%
+    write('build/artist-nodes-data.json')
 
-zip28 <- readOGR('~/Dev/glasstiredatahack/data/ZIP28_2017.shp')
+venue_nodes <- data.frame(
+                            id=unique(graph_dat$venue_name),
+                            group=2
+                )
 
-plot(zip28)
-symbols(venue_counts$lng, venue_counts$lat, circles=.0008*sqrt(venue_counts$venue_count), add=TRUE, inches=FALSE, bg="#00000050", lwd=0.5)
-
-
-
-
-
-
-
+venue_nodes %>%
+    jsonlite::toJSON(auto_unbox=T) %>%
+    write('build/venue-nodes-data.json')
